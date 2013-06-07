fa17cef6978097cf074496e70da3365d4a503222
Justin <rockjolt375@hotmail.com>
2013-06-07 01:25:21 -0400

    Refined java to php communication.

--- src/com/mythicacraft/registration/commands/Commands.java
+++ src/com/mythicacraft/registration/commands/Commands.java
@@ -4,6 +4,11 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 import java.net.URLConnection;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -17,11 +22,7 @@ import org.bukkit.entity.Player;
 import com.mythicacraft.registration.Registration;
 
 /* To do:
- *  - Database check that scans for duplicates of email/sendername
- *  - Add MC user name to profile
- *  - /register help
  *  - registration email
- *  - add information about logging in and modifying data on forums to confirm message
  */
 
 public class Commands implements CommandExecutor {
@@ -33,10 +34,16 @@ public class Commands implements CommandExecutor {
 	 }
 	
 	public boolean onCommand(CommandSender sender, Command cmd, String commandLabel, String[] args) {
-		Player player = (Player) sender;
+		final Player player = (Player) sender;
 		
 		if(commandLabel.equalsIgnoreCase("register") || commandLabel.equalsIgnoreCase("reg")){
 			if(!sender.hasPermission("register.registered")){
+				
+				if(args.length == 0 || args[0].equalsIgnoreCase("help") || args[0].equalsIgnoreCase("?")){
+					sender.sendMessage(ChatColor.GREEN + "-----" + ChatColor.GOLD + "Register Help" + ChatColor.GREEN + "-----");
+					sender.sendMessage(ChatColor.GOLD + "The purpose of /register is to automatically add your to our forums via convenient in-game commands. Our forums are in direct relation to our permissions system - so without registering you cant get promoted!");
+					sender.sendMessage(ChatColor.GOLD + "To get started, type '" + ChatColor.AQUA + "/register [email] [password] " + ChatColor.GOLD + "'.");
+				}
 				
 				if(args.length != 2){ //If player did not enter /register [email] [password] command format
 					sender.sendMessage(ChatColor.RED + "Please enter a valid email and password by typing '/register [email] [password]'");
@@ -60,7 +67,17 @@ public class Commands implements CommandExecutor {
 		
 		if(commandLabel.equalsIgnoreCase("confirm")){ //If player confirms information, execute to PHP script
 			if(plugin.emailHash.containsKey(player)){
-				      try {
+				try { //Checks database for duplicate email/username
+					if(!duplicateCheck(sender.getName(), plugin.emailHash.get(player))){
+						sender.sendMessage(ChatColor.RED + "Your username or email already exist on the forum!");
+						return true;
+					}
+				} catch (SQLException e1) {
+					e1.printStackTrace();
+				}
+				
+				    //Connect to external PHP script to add user to forum
+					try {
 				        URL phpUrl = new URL("http://www.mythicacraft.com/game2forum.php?username=" + sender.getName() + "&password=" + plugin.passHash.get(player) + "&email=" + plugin.emailHash.get(player));
 				        URLConnection urlCon = phpUrl.openConnection();
 				        BufferedReader br = new BufferedReader(
@@ -74,11 +91,21 @@ public class Commands implements CommandExecutor {
 				      } catch(Exception e) {
 				        // handle errors here...
 				      }
-				sender.sendMessage("Test message for confirming action");
-				Bukkit.getServer().getScheduler().cancelTask(Integer.parseInt(plugin.taskIDHash.get(player)));
+				sender.sendMessage(ChatColor.GREEN + "You have successfully registered! You may now visit our forums at " + ChatColor.AQUA + "www.mythicacraft.com/forums " + ChatColor.GREEN + "and log in with your in-game username and the password you specified!");
+				Bukkit.getServer().getScheduler().cancelTask(Integer.parseInt(plugin.taskIDHash.get(player))); //Cancels timeout task
+				
+				//Clears hash data to remove access to confirm and cancel commands
 				plugin.emailHash.remove(player);
 				plugin.passHash.remove(player);
 				plugin.taskIDHash.remove(player);
+				
+				//Schedules automatic sync with forums 10 seconds after registration confirmation
+				Bukkit.getServer().getScheduler().scheduleSyncDelayedTask(plugin, new Runnable() {
+					    public void run() {
+					    	player.performCommand("sync");	
+					    } //end void run()
+					   } //end bukkit start scheduler
+					   , 100L); 
 				return true;
 			}
 			else{
@@ -89,6 +116,8 @@ public class Commands implements CommandExecutor {
 		if(commandLabel.equalsIgnoreCase("cancel")){ //If player cancels process, restart.
 			if(plugin.emailHash.containsKey(player)){
 				sender.sendMessage(ChatColor.GOLD + "You have cancelled your registration.");
+				
+				//Cancels timeout and removes playerdata from hash
 				Bukkit.getServer().getScheduler().cancelTask(Integer.parseInt(plugin.taskIDHash.get(player)));
 				plugin.emailHash.remove(player);
 				plugin.passHash.remove(player);
@@ -128,5 +157,31 @@ public class Commands implements CommandExecutor {
 		 plugin.taskIDHash.put(player, Integer.toString(taskID));
 			   return true;
 	 }
+	 
+		public boolean duplicateCheck(String player, String email) throws SQLException { //Pulls player information from database to check for duplicates
+	        String sqlURL = "jdbc:mysql://box777.bluehost.com:3306/mythicac_forums";
+			Connection conn = DriverManager.getConnection(sqlURL, "mythicac_admin2", "WArrior11");
+			PreparedStatement emailStatement = conn.prepareStatement("SELECT * FROM phpbb_users WHERE user_email = '" + email + "'");
+			PreparedStatement usernameStatement = conn.prepareStatement("SELECT * FROM phpbb_profile_fields_data WHERE pf_minecraft_user = '" + player + "'"); //Put your query in the quotes
+			ResultSet emailSelect = emailStatement.executeQuery(); //Executes the query
+			ResultSet userSelect = usernameStatement.executeQuery();
+			
+			if(!emailSelect.next()){
+				if(!userSelect.next()){
+					emailSelect.close();
+					userSelect.close();
+					conn.close();	
+					return true;
+				}
+				emailSelect.close();
+				userSelect.close();
+				conn.close();	
+				return false;
+			}
+			emailSelect.close();
+			userSelect.close();
+			conn.close();			
+			return false;
+		}
 	 
 }//End Class
